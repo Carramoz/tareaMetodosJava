db33a682c1100328e6fa7742c05f99efe721b16b
Carlos Ramos <carramoz@hotmail.com>
2023-07-31 11:11:56 -0600

    Se agrega tarea de metodos en Java

--- src/Main.java
+++ src/Main.java
@@ -10,20 +10,29 @@ public class Main
         System.out.println( "Enter your last name:  " );
         String lastName = console.next();
         printNameCharacters( name );
-        printNameLength( name );
+        printlastNameCharacters( lastName );
+        printNameLength( name, lastName );
         printFullName(name, lastName);
         console.close();
     }
 
-    private static void printNameLength( String name )
+    private static void printNameLength( String name, String lastName )
     {
-        System.out.println("Name length is: "+(name.length()));
+        System.out.println("Name length is: "+(name.length()+lastName.length()));
     }
 
     private static void printNameCharacters( String name )
     {
     	for(int i = 0; i<name.length(); i++) {
     		System.out.println(name.charAt(i));
+    	}
+    	System.out.println();
+    }
+    
+    private static void printlastNameCharacters( String lastName )
+    {
+    	for(int i = 0; i<lastName.length(); i++) {
+    		System.out.println(lastName.charAt(i));
     	}
     	System.out.println();
     }
